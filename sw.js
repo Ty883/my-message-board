69810cc53571d24cc0ea04b2ab29327ab516b989
Ty883 <tyla.macgirl@gmail.com>
2026-05-12 21:47:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: 'https://img.icons8.com/fluency/192/000000/chat.png',
-        badge: 'https://img.icons8.com/fluency/72/000000/chat.png'
-    };
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// This listens for "Background Sync" or push events if you ever go full-push
+self.addEventListener('push', (event) => {
+    const options = { body: 'New Message Received!', icon: 'https://cdn-icons-png.flaticon.com/512/8943/8943377.png' };
+    event.waitUntil(self.registration.showNotification('Secret Board', options));
 });
